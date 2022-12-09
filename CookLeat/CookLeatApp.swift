ddd0960e43c1f12a6f4624ee9b62f054ba62fc4e
joaquinsarandeses <joaquinsarandeses@gmail.com>
2022-12-09 18:18:27 +0100

    Initial Commit

---
+++ CookLeat/CookLeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookLeatApp.swift
+//  CookLeat
+//
+//  Created by joaquin sarandeses on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CookLeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
