7c80a27409c16abf803ad02a1ea00fedeb17857c
joaquinsarandeses <joaquinsarandeses@gmail.com>
2023-01-25 17:47:02 +0100

    Primer cambio

--- CookLeat/ContentView.swift
+++ CookLeat/ContentView.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isActive: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        NavigationView{
+            ZStack {
+                if self.isActive{
+                    Text("Hola")
+                }else{
+                    Rectangle()
+                        .foregroundColor(Color("BackY"))
+                        .background(Color("BackY"))
+                    Image("Splash")
+                }
+                
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline:
+                        .now() + 2.5){
+                            withAnimation{
+                                self.isActive = true
+                            }
+                        }
+                
+            }
         }
-        .padding()
+        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
